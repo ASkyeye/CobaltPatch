6685c5f534d1a885c37f272203c5350a4f0cbffd
SecIdiot <ajhson001@gmail.com>
2020-09-03 08:38:36 -0500

    Patch: Patch in the length so the shellcode knows its size.

--- misc/pedump.py
+++ misc/pedump.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 import pefile
+import struct
 import sys
 
 def EntryPoint(argv):
@@ -16,10 +17,23 @@ def EntryPoint(argv):
             #
             fpe = pefile.PE(argv[1]);
             raw = fpe.sections[0].get_data()
-            
+    
+            #
+            # Calculates the end of code / jump
+            # so that we can jump into the mem
+            # dll.
+            #
+            # Furthermore patches the length so
+            # that the shellcode knows its size.
+            #
             ofs = raw.index(b'\xcc\xcc\xcc\xcc');
             raw = raw[:ofs];
+            raw = raw.replace(b'\x41\x41\x41\x41', struct.pack('<I', len(raw)));
 
+            #
+            # Writes the entire file into a bin
+            # for usage / processing.
+            #
             bin = open(argv[2], 'wb+');
             bin.write(raw);
             bin.close();
